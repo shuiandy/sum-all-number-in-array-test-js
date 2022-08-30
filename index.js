ba12167f8c37d50a967d2209df88b73b399f6af3
Shuai Hu <shuiandy@gmail.com>
2022-08-29 20:49:16 -0400

    initial commit

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-function sumItems(array) {
+const sumItems = function(array) {
   // Sum all the numbers in the array
-}
+  let res = 0;
+  const recurse = function(array) {
+    for (const item of array) {
+      if (Array.isArray(item)) {
+        recurse(item);
+      } else {
+        res += item;
+      }
+    }
+  };
+  recurse(array);
+  return res;
+};
 
 module.exports = sumItems;
